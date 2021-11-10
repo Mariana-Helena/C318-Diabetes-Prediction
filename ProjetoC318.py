bebcc757a212d72b263700680adf86087045871b
Mariana Helena Inês Moreira <mariana.helena@inatel.br>
2021-11-10 12:17:21 -0300

    initial commit

---
+++ ProjetoC318.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+# ============================================================================
+# Subject: Tópicos Especiais II (C318) 
+#
+# Theme: Diabetes Prediction
+# 
+# Students: Luana, Mariana, Sarah e Sinara
+#
+# Professor: Ricardo Augusto
+# ----------------------------------------------------------------------------
+import pandas as pd
+
+df = pd.read_csv('diabetes.csv')
+df.info() 
